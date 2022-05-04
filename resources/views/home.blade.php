269384c242fa598b0ae3b136189db3e1b2832248
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-05-04 16:11:52 +0200

    -aggiunto stile-

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,22 @@
 
 @section('content')
     <main>
-        <ul>
             @foreach ($movies as $movie)
-            <li>{{ $movie->title }}</li>
+                <div class="cards">
+                    <div class="card">
+                        <h6>Titolo:</h6>
+                        <h1>{{ $movie->title }}</h1>
+                        <h6>Titolo originalee:</h6>
+                        <h2>{{ $movie->original_title }}</h2>
+                        <h6>Nazionalità:</h6>
+                        <h3>{{ $movie->nationality }}</li>
+                        <h6>Anno d'uscita nelle sale:</h6>
+                        <h4>{{ $movie->date }}</h4>
+                        <h6>Voto:</h6>
+                        <h4>{{ $movie->vote }}</h4>
+                    </div>
+                </div>
             @endforeach
-        </ul>
+        
     </main>
 @endsection
