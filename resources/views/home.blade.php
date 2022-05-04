c33c42e50053b1e026d8f8fb89b6e441466344c6
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-05-04 16:15:03 +0200

    -aggiunto stile-

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,22 +4,21 @@
 
 @section('content')
     <main>
-            @foreach ($movies as $movie)
-                <div class="cards">
-                    <div class="card">
-                        <h6>Titolo:</h6>
-                        <h1>{{ $movie->title }}</h1>
-                        <h6>Titolo originalee:</h6>
-                        <h2>{{ $movie->original_title }}</h2>
-                        <h6>Nazionalità:</h6>
-                        <h3>{{ $movie->nationality }}</li>
-                        <h6>Anno d'uscita nelle sale:</h6>
-                        <h4>{{ $movie->date }}</h4>
-                        <h6>Voto:</h6>
-                        <h4>{{ $movie->vote }}</h4>
-                    </div>
+        @foreach ($movies as $movie)
+            <div class="cards">
+                <div class="card">
+                    <h6>Titolo:</h6>
+                    <h1>{{ $movie->title }}</h1>
+                    <h6>Titolo originalee:</h6>
+                    <h2>{{ $movie->original_title }}</h2>
+                    <h6>Nazionalità:</h6>
+                    <h3>{{ $movie->nationality }}</li>
+                    <h6>Anno d'uscita nelle sale:</h6>
+                    <h4>{{ $movie->date }}</h4>
+                    <h6>Voto:</h6>
+                    <h4>{{ $movie->vote }}</h4>
                 </div>
-            @endforeach
-        
+            </div>
+        @endforeach
     </main>
 @endsection
