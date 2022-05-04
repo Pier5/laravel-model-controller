cb987aa89f3a16432db0138d1a351b21035685ed
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-05-04 17:36:31 +0200

    -voti in stelline-

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,7 @@
                 <div class="card">
                     <h6>Titolo:</h6>
                     <h1>{{ $movie->title }}</h1>
+
                     <h6>Titolo originalee:</h6>
                     <h2>{{ $movie->original_title }}</h2>
                     
@@ -22,9 +23,16 @@
                     <h6>Anno d'uscita nelle sale:</h6>
                     <h4>{{ date_format(date_create($movie->date), 'Y') }}</h4>
 
-                    
                     <h6>Voto:</h6>
-                    <h4>{{ $movie->vote }}</h4>
+                    <div class="stars">
+                        @for ($i = 0; $i < ceil($movie->vote / 2); $i++) 
+                            <i class="fullstar fa-solid fa-star"></i>
+                         @endfor
+                        @for ($j = 0; $j < floor((10 - $movie->vote) / 2); $j++)
+                            <i class="emptystar fa-solid fa-star"></i>   
+                        @endfor                    
+                    </div>
+                        
                 </div>
             </div>
         @endforeach
